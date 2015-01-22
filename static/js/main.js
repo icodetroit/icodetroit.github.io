aab1b33c7221b5cf5e33a7ae0d4d21f286b91ab4
migreva <mikevee313@gmail.com>
2015-01-22 00:19:45 -0500

    Non-skrollr layout

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,15 @@
 requirejs.config({
 	baseUrl: "/static",
+	shim: {
+		"bootstrap": ["jquery"]
+	},
 	paths: {
-		"skrollr": "bower_components/skrollr/dist/skrollr.min",
-		"jquery": "bower_components/jquery/dist/jquery.min"
+		"jquery": "bower_components/jquery/dist/jquery.min",
+		"bootstrap": "bower_components/bootstrap/dist/js/bootstrap.min"
 	}
 });
 
-requirejs(["jquery", "skrollr"], function($, skrollr){
-	var s = skrollr.init();
+requirejs(["jquery", "bootstrap"], function($, bootstrap) {
+	// $(".header-bar").affix();
 
 });
