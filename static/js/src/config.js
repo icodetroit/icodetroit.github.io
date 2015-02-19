dc154d03908f01b7ff7ffe48e61d3c4ee7b10c4b
migreva <mikevee313@gmail.com>
2015-02-18 23:57:59 -0500

    Bootstrap js on subpages

--- static/js/src/config.js
+++ static/js/src/config.js
@@ -4,10 +4,12 @@ requirejs.config({
 		jquery: {
 			exports: "jQuery"
 		},
-		"velocity": ["jquery"]
+		"velocity": ["jquery"],
+		"bootstrap": ["jquery"]
 	},
 	paths: {
 		"jquery": "/static/bower_components/jquery/dist/jquery.min",
 		"velocity": "/static/bower_components/velocity/velocity.min",
+		"bootstrap": "/static/bower_components/bootstrap/dist/js/bootstrap.min"
 	}
 });
---
+++ static/js/src/subpage.js
@@ -0,0 +1,7 @@
+requirejs(["config"], function() {
+	requirejs(["jquery", "bootstrap"], function($, bootstrap) {
+
+	});
+});
+
+
